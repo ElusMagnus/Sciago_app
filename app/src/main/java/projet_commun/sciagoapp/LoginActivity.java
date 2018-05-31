e188dfc24e53f3c0e4b2275d18c414ecc489fbc1
Elie Abou-Haydar <elie.ah92@gmail.com>
2018-05-31 22:32:25 +0200

    Travail préparatoire pour implémentation BDD.

--- app/src/main/java/projet_commun/sciagoapp/LoginActivity.java
+++ app/src/main/java/projet_commun/sciagoapp/LoginActivity.java
@@ -101,6 +101,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
+        Button mRegisterButton = (Button) findViewById(R.id.email_register_button);
+        mRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+            }
+        });
+
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
---
+++ app/src/main/java/projet_commun/sciagoapp/RegisterActivity.java
@@ -0,0 +1,119 @@
+package projet_commun.sciagoapp;
+
+/**
+ * Created by Elie on 31/05/2018.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import projet_commun.sciagoapp.utils.JSONParser;
+
+public class RegisterActivity extends Activity {
+
+    JSONParser jsonParser = new JSONParser();
+    EditText inputUsername;
+    EditText inputPassword;
+
+    // url to create new product
+    private static String url_create_user = "http://eabouhaydar.ddns.net/sciago/create-user.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_user);
+
+        // Edit Text
+        inputUsername = (EditText) findViewById(R.id.inputUsername);
+        inputPassword = (EditText) findViewById(R.id.inputPassword);
+
+        // Create button
+        Button btnCreateProduct = (Button) findViewById(R.id.btnCreateUser);
+
+        // button click event
+        btnCreateProduct.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // creating new product in background thread
+                new CreateNewProduct().execute();
+            }
+        });
+    }
+
+    /**
+     * Background Async Task to Create new product
+     * */
+    class CreateNewProduct extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+                    }
+
+        /**
+         * Creating product
+         * */
+        protected String doInBackground(String... args) {
+            String name = inputUsername.getText().toString();
+            String price = inputPassword.getText().toString();
+
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("username", name));
+            params.add(new BasicNameValuePair("password", price));
+
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject json = jsonParser.makeHttpRequest(url_create_user,
+                    "GET", params);
+            // check log cat fro response
+            Log.d("Create Response", json.toString());
+
+            return json.toString();
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            JSONObject json;
+            try {
+                json = new JSONObject(result);
+                int success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    // successfully created product
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                    // closing this screen
+                    finish();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
